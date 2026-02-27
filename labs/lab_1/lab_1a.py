d9d8408f5a1340b434afe150265e442853ab0616
kou62 <kennyou429@gmail.com>
2026-02-26 20:07:49 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Kenny Ou" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("hello my name is kenny")
 if __name__ == "__main__":
     main()
